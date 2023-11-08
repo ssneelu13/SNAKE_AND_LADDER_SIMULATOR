92978272756bc2e29ddecdd669c8af9b54c860aa
Neelufer <ssneelu13@gmail.com>
2023-11-08 22:55:58 +0530

    The Player rolls the die to get a number 1-6

--- src/SNAKE_AND_LADDER_USECASES/Snake_And_Ladder.java
+++ src/SNAKE_AND_LADDER_USECASES/Snake_And_Ladder.java
@@ -1,9 +1,15 @@
 package SNAKE_AND_LADDER_USECASES;
+import java.util.Random;
 public class Snake_And_Ladder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         int player_position=0; //starting position of player
         int winning_position=100; //usecase1:Snake and Ladder game played with single player at start position 0
+        Random random = new Random();
+        while(player_position<winning_position){
+            int diceRoll = random.nextInt(6) + 1; //usecase2:The Player rolls the die to get a number 1-6
+            
+        }
 
         }
 
