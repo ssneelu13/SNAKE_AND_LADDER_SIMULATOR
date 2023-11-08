c2d1261faf28d584f07aa10f9c3660f0ae9ffc7a
Neelufer <ssneelu13@gmail.com>
2023-11-08 18:27:41 +0530

    Welcome To Snake & Ladder Simulator Program

---
+++ src/SNAKE_AND_LADDER_USECASES/Snake_And_Ladder.java
@@ -0,0 +1,7 @@
+package SNAKE_AND_LADDER_USECASES;
+
+public class Snake_And_Ladder {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake & Ladder Simulator Program");
+    }
+}
