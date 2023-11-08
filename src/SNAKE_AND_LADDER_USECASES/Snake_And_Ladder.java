b29b9a040e25518d339f7683876190a5031fa89b
Neelufer <ssneelu13@gmail.com>
2023-11-08 22:46:42 +0530

    Snake and Ladder game played with single player at start position 0

--- src/SNAKE_AND_LADDER_USECASES/Snake_And_Ladder.java
+++ src/SNAKE_AND_LADDER_USECASES/Snake_And_Ladder.java
@@ -1,7 +1,10 @@
 package SNAKE_AND_LADDER_USECASES;
-
 public class Snake_And_Ladder {
     public static void main(String[] args) {
-        System.out.println("Welcome To Snake & Ladder Simulator Program");
+        System.out.println("Welcome to Snake and Ladder Game");
+        int player_position=0; //starting position of player
+        int winning_position=100; //usecase1:Snake and Ladder game played with single player at start position 0
+
+        }
+
     }
-}
